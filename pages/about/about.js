7362ad572ac13c343480669fca1f14d4d34adf2d
xuyongqin <1021151991@qq.com>
2022-02-23 11:25:08 +0800

    回到 v1.2.0_20220222

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,4 +1,4 @@
-// v1.2.0_20220222
+// v1.2.0_20220222 
 const app = getApp();
 Page({
   data: {
@@ -6,20 +6,10 @@ Page({
     CustomBar: app.globalData.CustomBar,
     ColorList: app.globalData.ColorList,
   },
-  onLoad: function () {},
+  onLoad: function () { },
   pageBack() {
     wx.navigateBack({
       delta: 1
     });
-  },
-  //图片点击事件
-  enlarge: function (event) {
-    var src = event.currentTarget.dataset.src; //获取data-src
-    var imgList = [src];
-    //图片预览
-    wx.previewImage({
-      current: src, // 当前显示图片的http链接
-      urls: imgList // 需要预览的图片http链接列表
-    })
-  },
+  }
 });
